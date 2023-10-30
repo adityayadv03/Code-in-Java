9f50d028d9bccc78b45af5e97bcf012195ed1fc3
adityayadv03 <149312402+adityayadv03@users.noreply.github.com>
2023-10-31 00:18:15 +0530

    Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -7,9 +7,9 @@ public class Reverse //Program To Reverse a Number Or String
         Scanner in = new Scanner(System.in);
         System.out.print("Enter Any Number or String To Reverse :- "); //Taking Input From the User
         String str=in.nextLine();
-        int len=str.length();
+        int length=str.length();
         String nstr=""; // Created a New String to Store the Solution
-        for(int i=0 ; i<len ; i++)
+        for(int i=0 ; i<length ; i++)
         {
             char ch=str.charAt(i); // Taking the First Character and storing it
             nstr=ch+nstr; // Adding the Character to the Starting of the String 
