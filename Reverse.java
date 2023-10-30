8ce08803402460025ecc167ad692d42a3ca61218
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-10-31 00:19:40 +0530

    Given Meaningfull Name To variable

--- Reverse.java
+++ Reverse.java
@@ -6,15 +6,15 @@ public class Reverse //Program To Reverse a Number Or String
     {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter Any Number or String To Reverse :- "); //Taking Input From the User
-        String str=in.nextLine();
-        int len=str.length();
-        String nstr=""; // Created a New String to Store the Solution
-        for(int i=0 ; i<len ; i++)
+        String string=in.nextLine();
+        int length=string.length();
+        String newstring=""; // Created a New String to Store the Solution
+        for(int i=0 ; i<length ; i++)
         {
-            char ch=str.charAt(i); // Taking the First Character and storing it
-            nstr=ch+nstr; // Adding the Character to the Starting of the String 
+            char ch=string.charAt(i); // Taking the First Character and storing it
+            newstring=ch+newstring; // Adding the Character to the Starting of the String 
         }
-        System.out.println("After Reversing :- "+nstr);
+        System.out.println("After Reversing :- "+newstring);
         in.close();
     }
 }
