72af4ab78146108cbdcb246dd70eeea156768033
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-02 18:34:20 +0530

    Adding Comment

--- Char_In_NewLine.java
+++ Char_In_NewLine.java
@@ -1,16 +1,17 @@
+/*Author :- Aditya Yadav */
 import java.util.*;
 public class Char_In_NewLine
 {
     public static void main(String[]args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter Any String :- ");
+        System.out.print("Enter Any String :- "); //Taken Input From User
         String str=in.nextLine();
         System.out.println("Character in NewLine :- ");
-        for(int i=0 ; i<str.length() ; i++)
+        for(int i=0 ; i<str.length() ; i++) //Covering Every Element Using Loop
         {
-            char ch=str.charAt(i);
-            System.out.println(ch);
+            char ch=str.charAt(i); //Storing Each Character
+            System.out.println(ch); //Printing Each Character in New Line
         }
         in.close();
     }
