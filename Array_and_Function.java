56b4037debb94ed3ccf115209328c968447b4c59
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-03 19:15:43 +0530

    Array and Functions

--- Array_and_Function.java
+++ Array_and_Function.java
@@ -1,7 +1,8 @@
+/*Author :- Aditya Yadav */
 import java.util.*;
-public class Array_and_Function
+public class Array_and_Function //Implement Basic Code Of Array With Functions
 {
-    public static void display(int ar[] , int lim)
+    public static void display(int ar[] , int lim) //Function to Display the Array
     {
         System.out.print("Element Of Array :- ");
         for(int i=0 ; i<=lim ; i++)
@@ -13,14 +14,14 @@ public class Array_and_Function
     public static void main(String[]args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter The Limit :- ");
+        System.out.print("Enter The Limit :- "); //Taking the Number Of Element for Array Size
         int n=in.nextInt();
-        int arr[] = new int[n];
+        int arr[] = new int[n]; //Declaring Array
         for(int i=0 ; i<n ; i++)
         {
             System.out.print("Enter The Element :- ");
-            arr[i]=in.nextInt();
-            display(arr,i);
+            arr[i]=in.nextInt(); //Taking Input
+            display(arr,i); //Displaying the Element Present Till the Current Stage
         }
         in.close();
     }
