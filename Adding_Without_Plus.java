e2ab597eb39b87afa53b978c46a4561ec74b1380
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-06 18:37:20 +0530

    Adding Without + Operator

--- Adding_Without_Plus.java
+++ Adding_Without_Plus.java
@@ -1,9 +1,10 @@
+/*Author :- Aditya Yadav */
 import java.util.*;
-public class Adding_Without_Plus
+public class Adding_Without_Plus //Program to Add Two Number Without Using + operator
 {
-    public static int fun_to_add(int x,int y)
+    public static int fun_to_add(int x,int y) //Function to Add Number returning the Modified Number
     {
-        for(int i=1 ; i<=y ; i++)
+        for(int i=1 ; i<=y ; i++) //Running The Loop To Second Number Times
         {
             x++;
         }
@@ -12,11 +13,11 @@ public class Adding_Without_Plus
     public static void main(String[]args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter First Number :- ");
+        System.out.print("Enter First Number :- "); //Taking User From User
         int a=in.nextInt();
         System.out.print("Enter Second Number :- ");
         int b=in.nextInt();
-        System.out.println("Add :- "+fun_to_add(a,b));
+        System.out.println("Add :- "+fun_to_add(a,b)); //Printing the Output
         in.close();
     }
 }
