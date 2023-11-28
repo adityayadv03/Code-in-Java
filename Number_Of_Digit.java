06e8cd2e59b16bcb5ccb65ef82f71e7e565fffe5
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-28 08:02:05 +0530

    Counting Digit

--- Number_Of_Digit.java
+++ Number_Of_Digit.java
@@ -1,18 +1,19 @@
+/*Author :- Aditya Yadav */
 import java.util.*;
-public class Number_Of_Digit
+public class Number_Of_Digit //Program to Count the Digit in Given Number
 {
     public static void main(String[]args)
     {
         int co=0;
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter The Number :- ");
+        System.out.print("Enter The Number :- "); //Taking Number Of From User
         int n=in.nextInt();
-        while(n!=0)
+        while(n!=0) //Running The Loop Till the Number become zero
         {
-            co++;
+            co++; //Increasing the digit counter with the loop
             n/=10;
         }
-        System.out.println("Number of Digit :- "+co);
+        System.out.println("Number of Digit :- "+co); //Printing The Counted Digit
         in.close();
     }
 }
