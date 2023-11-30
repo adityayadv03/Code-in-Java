63d9aeec2601b3f7904eadf52456a88cac57864f
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-30 19:39:04 +0530

    Factorial By Recursion

--- Factorial_Recursion.java
+++ Factorial_Recursion.java
@@ -1,9 +1,10 @@
+/*Author :- Aditya Yadav */
 import java.util.*;
-public class Factorial_Recursion
+public class Factorial_Recursion //Code To Find Factorial of First 20 Number 
 {
-    public static long fac(long x)
+    public static long fac(long x)//A Recursive Method To Find the Factorial
     {
-        if(x==1 || x==0)
+        if(x==1 || x==0) //Base Condition
         {
             return 1;
         }
@@ -15,9 +16,9 @@ public class Factorial_Recursion
     public static void main(String[]args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter a Number :- ");
-        long n=in.nextLong();
-        System.out.println("Factorial :- "+fac(n));
+        System.out.print("Enter a Number :- "); //taking Input
+        long n=in.nextLong();//Using long to store as big number as possible
+        System.out.println("Factorial :- "+fac(n)); //Printing the Return Factorial
         in.close();
     }
 }
