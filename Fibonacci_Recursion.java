667844353f8284cbd8b7970036f3c5348b8664c4
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-01 19:16:23 +0530

    Fibonacci Series Using Recursion

--- Fibonacci_Recursion.java
+++ Fibonacci_Recursion.java
@@ -1,9 +1,10 @@
+/*Author :- Aditya Yadav */
 import java.util.*;
-public class Fibonacci_Recursion
+public class Fibonacci_Recursion //Code To Find Fibonacci Series
 {
-    public static int fib(int x)
+    public static int fib(int x) //Function to Find the ith Element of Series Using Recursion
     {
-        if(x<=1)
+        if(x<=1) //Base Condition
         {
             return x;
         }
@@ -15,12 +16,12 @@ public class Fibonacci_Recursion
     public static void main(String[]args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the Limit :- ");
+        System.out.print("Enter the Limit :- "); //Taking Input of Series length
         int n=in.nextInt();
         System.out.print("Fibonacci Series :- ");
         for(int i=0 ; i<n ; i++)
         {
-            System.out.print(fib(i)+" ");
+            System.out.print(fib(i)+" "); //Printing the Series
         }
         in.close();
     }
