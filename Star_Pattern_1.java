4937ef28b2bfc728a2551229e4fddd95c55a7578
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-03 19:11:24 +0530

    Star Pattern

--- Star_Pattern_1.java
+++ Star_Pattern_1.java
@@ -1,23 +1,24 @@
+/*Author :- Aditya Yadav */
 import java.util.*;
-public class Star_Pattern_1
+public class Star_Pattern_1 //Program to Print A Basic Star Function
 {
-    public static void star(int n)
+    public static void star(int n) //Function To Print the Pattern For a Given Limit
     {
-        for(int i=1 ; i<=n ; i++)
+        for(int i=1 ; i<=n ; i++) //Loop For Each Row
         {
-            for(int j=1 ; j<=i ; j++)
+            for(int j=1 ; j<=i ; j++) //Loop For Each Column
             {
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println(); //For Changing Line
         }
     }
     public static void main(String[]args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the Limit :- ");
+        System.out.print("Enter the Limit :- "); //Taking the Limit
         int n=in.nextInt();
-        star(n);
+        star(n); //Calling The Require Function
         in.close();
     }
 }
