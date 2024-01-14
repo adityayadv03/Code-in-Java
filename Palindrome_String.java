d817c44d96f874406979fb3e515c2f68c6739551
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-01-14 20:05:21 +0530

    Palindrome String Completed

--- Palindrome_String.java
+++ Palindrome_String.java
@@ -1,29 +1,29 @@
 /*Author :- Aditya Yadav */
 import java.util.*;
 public class Palindrome_String //Program to Check whether a given string palindrome or not
-{
-    public static boolean CheckPalindromeString(String str)
+{ //palindrome means the String after reversing will remain same
+    public static boolean CheckPalindromeString(String str) //Function which check the String for palindrome and return true or false
     {
-        for(int i=0 ; i<str.length()/2 ; i++)
+        for(int i=0 ; i<str.length()/2 ; i++) //traversing the String
         {
-            if(str.charAt(i)!=str.charAt(str.length()-i-1))
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)) //if the 1 character and last index is not same then the function return false and so on for the other i value
             {
                 return false;
             }
         }
-        return true;
+        return true; //If the Condition in the if block dont encounter then this statement will tell the user that the String is Palindromic
     }
-    public static void main(String[] args)
+    public static void main(String[] args) //Main Function
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter a String :- ");
+        System.out.print("Enter a String :- "); //Taking any String from User
         String str=in.nextLine();
-        boolean ch=CheckPalindromeString(str);
-        if(ch==true)
+        boolean ch=CheckPalindromeString(str); //Passing the String to the above function
+        if(ch==true) //if the function return true then the string is palindromic
         {
             System.out.println("The String is Palindrome . ");
         }
-        else
+        else //if the function return false then the string is Not palindrome
         {
             System.out.println("The String is not Palindrome . ");
         }
